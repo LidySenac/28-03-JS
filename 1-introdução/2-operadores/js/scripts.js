97bc7ae76b16c9d57305ac220f3d797c9f2698e1
LIDIAN PAREDES DE OLIVEIRA <lidian.poliveira@senacsp.edu.br>
2024-03-28 14:50:50 -0300

    idade JS

---
+++ 1-introdução/2-operadores/js/scripts.js
@@ -0,0 +1,78 @@
+console.log(typeof 2)
+console.log(typeof 2.2)
+console.log(typeof-4)
+console.log("Lidian")
+console.log(typeof "Lidian")
+
+// 2-operações arimetrica
+
+console.log(2+4)
+console.log(10 -5)
+console.log(5*4)
+console.log(10/2)
+
+// 3- Special numbers
+
+console.log(typeof Infinity)
+
+console.log(12 * "asd")
+
+// 4- Strings
+
+console.log("Um Texto")
+console.log('outro texto')
+console.log(`Mais um texto`)
+console.log(typeof "Um Texto")
+console.log(typeof 'outro texto')
+console.log(typeof `Mais um texto`)
+
+// 5- Síbolos especiais em strings
+
+console.log("Testando \n quebra de linha")
+console.log("Espaçamento \t de tab")
+
+// 6 - Concatenação
+
+console.log("Oi ," + "tudo" + "bem?")
+
+console.log(`Testando uma ` + "concatenação")
+
+// 7- Interpolação
+
+console.log(`a soma de 2 + 2 é: ${2+2}`)
+
+console.log("a soma de 2 + 2 é:" + (2+2))
+
+console.log(`Podemos executar qualquer coisa ${console.log('teste 🙅‍♀️')}`)
+
+// 08- Booleans
+
+console.log(5 > 2)
+
+console.log(typeof(5 > 2))
+
+console.log(2 > 5)
+
+console.log(10 == 10)
+
+console.log(10 == '10')
+
+console.log(10 === '10')
+
+console.log(10 == 9)
+
+console.log(10 != 9)
+
+console.log(10 != '9')
+
+console.log(10 !== '9')
+
+// 09- op logico na prática
+
+console.log(true && true)
+
+console.log(true && false)
+
+console.log(5>2 && 2<10)
+
+console.log(10>2 || 5>6 )
